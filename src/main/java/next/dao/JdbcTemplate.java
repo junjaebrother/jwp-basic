7fa4abe5afdea1cbb0cd755c372a4b70b24c6a4b
한재준 <gkswowns25@naver.com>
2025-07-30 02:38:06 +0900

    7th class: Exception

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -1,6 +1,7 @@
 package next.dao;
 
 import core.jdbc.ConnectionManager;
+import next.dao.exception.DataAccessException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,54 +11,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcTemplate {
-    public List query(String sql, PreparedStatementSetter preSet, RowMapper rowMapper) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+    public List query(String sql, PreparedStatementSetter preSet, RowMapper rowMapper) throws DataAccessException {
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql);
+             ){
             preSet.values(pstmt);
+            try (ResultSet rs = pstmt.executeQuery()){
+                List<Object> users = new ArrayList<>();
 
-            rs = pstmt.executeQuery();
-            List<Object> users = new ArrayList<>();
-
-            while(rs.next()) {
-                users.add(rowMapper.mapRow(rs));
+                while(rs.next()) {
+                    users.add(rowMapper.mapRow(rs));
+                }
+                return users;
             }
-
-            return users;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 
-    public void update(String sql, PreparedStatementSetter preSet) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+    public void update(String sql, PreparedStatementSetter preSet) throws DataAccessException {
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
             preSet.values(pstmt);
-
             pstmt.executeUpdate();
-        }finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 }
