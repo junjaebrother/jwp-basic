dd62ae6fb7b2edefe6d6e497d6f36a45ff31ad9a
한재준 <gkswowns25@naver.com>
2025-07-31 05:40:04 +0900

    8th class: model and view

---
+++ src/main/java/core/mvc/JspView.java
@@ -0,0 +1,40 @@
+package core.mvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Set;
+
+public class JspView implements View{
+    private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
+
+    private String viewName;
+
+    public JspView(String viewName) {
+        if (viewName == null) {
+            throw new NullPointerException("viewName is null");
+        }
+        this.viewName = viewName;
+    }
+
+
+    @Override
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception{
+        if (viewName.startsWith(DEFAULT_REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        Set<String> keys = model.keySet();
+        for (String key : keys) {
+            request.setAttribute(key, model.get(key));
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher(viewName);
+        rd.forward(request, response);
+    }
+}
