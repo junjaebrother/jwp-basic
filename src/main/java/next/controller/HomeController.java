ed321b9df92cb27f5d4c316fe38841af7ec3ed6a
한재준 <gkswowns25@naver.com>
2025-07-31 17:06:57 +0900

    8th class: refac 2

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -13,6 +13,6 @@ public class HomeController extends AbstractController {
     @Override
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         QuestionDao questionDao = new QuestionDao();
-        return jspView("home.jsp").addObject("questions", questionDao);
+        return jspView("home.jsp").addObject("questions", questionDao.findAll());
     }
 }
--- src/main/java/next/controller/qna/AddAnswerController.java
+++ src/main/java/next/controller/qna/AddAnswerController.java
@@ -16,13 +16,14 @@ import next.model.Answer;
 public class AddAnswerController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(AddAnswerController.class);
 
+    private AnswerDao answerDao = new AnswerDao();
+
     @Override
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         Answer answer = new Answer(req.getParameter("writer"), req.getParameter("contents"),
                 Long.parseLong(req.getParameter("questionId")));
         log.debug("answer : {}", answer);
 
-        AnswerDao answerDao = new AnswerDao();
         Answer savedAnswer = answerDao.insert(answer);
         return jsonView().addObject("answer", savedAnswer);
     }
