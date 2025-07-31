73a719ba44084bd960a98f90bf8f78f5db3ede5e
한재준 <gkswowns25@naver.com>
2025-07-31 16:52:18 +0900

    8th class: refac

--- src/main/java/next/controller/user/UpdateUserController.java
+++ src/main/java/next/controller/user/UpdateUserController.java
@@ -3,6 +3,8 @@ package next.controller.user;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,11 +13,11 @@ import next.controller.UserSessionUtils;
 import next.dao.UserDao;
 import next.model.User;
 
-public class UpdateUserController implements Controller {
+public class UpdateUserController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(UpdateUserController.class);
 
     @Override
-    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         UserDao userDao = new UserDao();
         User user = userDao.findByUserId(req.getParameter("userId"));
         if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
@@ -26,6 +28,7 @@ public class UpdateUserController implements Controller {
                 req.getParameter("email"));
         log.debug("Update User : {}", updateUser);
         user.update(updateUser);
-        return "redirect:/";
+
+        return jspView("redirect:/");
     }
 }
