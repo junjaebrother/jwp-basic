5d2082cae0c651a9a0be923837270a168b9df80e
한재준 <gkswowns25@naver.com>
2025-08-07 02:57:59 +0900

    9th class 8

--- src/main/java/next/controller/qna/AddAnswerController.java
+++ src/main/java/next/controller/qna/AddAnswerController.java
@@ -3,6 +3,8 @@ package next.controller.qna;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import next.dao.QuestionDao;
+import next.model.Question;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +17,7 @@ public class AddAnswerController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(AddAnswerController.class);
 
     private AnswerDao answerDao = new AnswerDao();
+    private QuestionDao questionDao = new QuestionDao();
 
     @Override
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
@@ -23,6 +26,10 @@ public class AddAnswerController extends AbstractController {
         log.debug("answer : {}", answer);
 
         Answer savedAnswer = answerDao.insert(answer);
-        return jsonView().addObject("answer", savedAnswer);
+        questionDao.addComment(Long.parseLong(req.getParameter("questionId")));
+
+        Question question = questionDao.findById(Long.parseLong(req.getParameter("questionId")));
+        ModelAndView modelAndView = jsonView().addObject("question", question);
+        return modelAndView.addObject("answer", savedAnswer);
     }
 }
